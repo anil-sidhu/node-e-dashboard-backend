4e0422e0f4eab5e483aaf048585389ffb786376c
anil sidhu <webanilsidhu@gmail.com>
2021-12-16 19:58:20 +0530

    done with basic

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express= require("express");
+const mongoose= require('mongoose');
 const app = express();
-app.get("/",(req, resp)=>{
-resp.send("app is working...")
-});
+const connectDB =async ()=>{
+    mongoose.connect("mongodb://localhost:27017/e-comm");
+    const productSchema= new mongoose.Schema({});
+    const product= mongoose.model("products",productSchema);
+    const data = await product.find();
+    console.warn(data);
+}
+
+connectDB();
 
 app.listen(5000);
