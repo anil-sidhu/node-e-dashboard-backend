a54e460163e5d357dbdb81d926205ff11177b74c
anil sidhu <webanilsidhu@gmail.com>
2022-01-04 21:06:17 +0530

    basic

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const express= require("express");
+const express = require("express");
+const cors = require("cors");
 require("./db/config");
 const User = require('./db/User');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.post("/register", async (req, resp)=>{
+app.post("/register", async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password
     resp.send(result);
 })
 
+
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user)
+        } else {
+            resp.send({ result: "No User found" })
+        }
+    } else {
+        resp.send({ result: "No User found" })
+    }
+
+})
 
 app.listen(5000);
