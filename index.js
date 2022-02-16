9e20971c9ddfe5f49526909406ff836fb8145138
anil sidhu <webanilsidhu@gmail.com>
2022-02-16 22:14:19 +0530

    Done with search api

--- index.js
+++ index.js
@@ -49,13 +49,46 @@ app.delete("/product/:id", async (req, resp) => {
     resp.send(result)
 }),
 
-app.get("/product/:id",async (req, resp)=>{
-    let result  = await Product.findOne({_id:req.params.id})
-    if(result){
-        resp.send(result)
-    }else{
-        resp.send({"result":"No Record Found."})
-    }
+    app.get("/product/:id", async (req, resp) => {
+        let result = await Product.findOne({ _id: req.params.id })
+        if (result) {
+            resp.send(result)
+        } else {
+            resp.send({ "result": "No Record Found." })
+        }
+    })
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        { $set: req.body }
+    )
+    resp.send(result)
+});
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        { $set: req.body }
+    )
+    resp.send(result)
+});
+
+app.get("/search/:key", async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            {
+                name: { $regex: req.params.key }  
+            },
+            {
+                company: { $regex: req.params.key }
+            },
+            {
+                category: { $regex: req.params.key }
+            }
+        ]
+    });
+    resp.send(result);
 })
 
 app.listen(5000);
