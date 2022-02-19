416fa034858abade62d6ac5624feeb2b0a77e589
anil sidhu <webanilsidhu@gmail.com>
2022-02-20 00:24:40 +0530

    Done with token

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 require("./db/config");
 const User = require('./db/User');
 const Product = require("./db/Product")
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-com';
 const app = express();
 
 app.use(express.json());
@@ -13,14 +15,24 @@ app.post("/register", async (req, resp) => {
     let result = await user.save();
     result = result.toObject();
     delete result.password
-    resp.send(result);
+    Jwt.sign({result}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+        if(err){
+            resp.send("Something went wrong")  
+        }
+        resp.send({result,auth:token})
+    })
 })
 
 app.post("/login", async (req, resp) => {
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
-            resp.send(user)
+            Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+                if(err){
+                    resp.send("Something went wrong")  
+                }
+                resp.send({user,auth:token})
+            })
         } else {
             resp.send({ result: "No User found" })
         }
